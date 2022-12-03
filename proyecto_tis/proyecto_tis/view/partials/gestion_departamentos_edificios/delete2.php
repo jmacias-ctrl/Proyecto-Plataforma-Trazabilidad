6debb958dd1b37bfadece0c4b0abb099ceef7e36
jmacias-ctrl <69021407+jmacias-ctrl@users.noreply.github.com>
2022-12-03 00:21:22 -0300

    Delete proyecto_tis/proyecto_tis directory

--- proyecto_tis/proyecto_tis/view/partials/gestion_departamentos_edificios/delete2.php
+++
@@ -1,9 +0,0 @@
-<?php
-require('view\partials\gestion_departamentos_edificios\conexion.php');
-
-$id_consultado= $_GET["id_departamento"];
-
-$sql="DELETE FROM departamentos WHERE id_departamento='$id_consultado'";
-$resultado= mysqli_query($conexion, $sql);
-
-?>
