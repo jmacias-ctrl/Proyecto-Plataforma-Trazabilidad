672e4a6e1ddee589db805c49605eb151c30bda97
jmacias-ctrl <69021407+jmacias-ctrl@users.noreply.github.com>
2022-12-04 23:01:46 -0300

    Delete proyecto_tis/view/partials/modules/databases directory

--- proyecto_tis/view/partials/modules/databases/read/read_events.php
+++
@@ -1,7 +0,0 @@
-<?php
-    require('view\partials\modules\databases\conexion.php');
-    require('view\partials\modules\databases\config.php');
-
-    require("view\partials\modules\databases\kcalendar_main.php");
-
-?>
